d5e43587fd3190d16283d9551dbe95390079a08d
Syed <Irfan.Syed@elektrobit.com>
2018-12-06 11:16:40 +0530

    added chat multi user

--- server.js
+++ server.js
@@ -13,12 +13,23 @@ const wss = new SocketServer({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+      ws.on('message', function (message) {
+        message = JSON.parse(message);
+        if (message.type === "name") {
+            ws.personName = message.data;
+            return;
+        }
+
+        console.log("recived: " + message);
+        wss.clients.forEach(function e(client) {
+            if (client !== ws) {
+                client.send(JSON.stringify({
+                    name: ws.personName,
+                    data: message.data
+                }));
+            }
+        })
+        // ws.send("From Server"+message);
+    });
   ws.on('close', () => console.log('Client disconnected'));
 });
-
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send("Hello");
-    // client.send(new Date().toTimeString());
-  });
-}, 1000);
